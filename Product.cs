4dba3fbfa982a2269d5f54b2cc6d67a6d38bddaa
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-12 01:56:34 +0300

    This program calculates the price of products after tax and discounts

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    class Product
+    {
+        internal string Name { get; set; }
+        internal int UPC { get; set; }
+        internal double Price { get; set; }
+        internal double UPCDiscount { get; set; }
+
+
+        public Product()
+        { }
+
+        public Product(string name, int UPC, double price, double UPCDiscount)
+        {
+            this.Name = name;
+            this.UPC = UPC;
+            this.Price = price;
+            this.UPCDiscount = UPCDiscount;
+
+        }
+    }
+}
