a5ee170a2dc55fee89dfb4e20102ce7288ec292a
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-12 08:53:18 +0300

    Add tax to the original price, then print orginal price and price after tax

---
+++ PriceCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    
+    class PriceCalculator
+    {
+        private double _taxAmount;
+        private Product _purchasedProduct;
+
+        public PriceCalculator()
+        {
+
+        }
+
+        public PriceCalculator(double tax, Product product)
+        {
+            this._purchasedProduct = product;
+            this._taxAmount = tax;
+
+        }
+
+        public double TaxCalculator()
+        {
+               double priceAferTax= this._purchasedProduct.Price + (this._purchasedProduct.Price * this._taxAmount);
+               return Math.Round(priceAferTax, 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace PriceCalculatorKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product Book = new Product("The Little Prince Book", 12345, 20.25, 0.07);
+            Console.WriteLine("Please Enter Tax amount:");
+            PriceCalculator price = new PriceCalculator(Double.Parse(Console.ReadLine()),Book);
+            Console.WriteLine($"The original price is {Book.Price}");
+            Console.WriteLine($"The price after tax is {price.TaxCalculator()}");
         }
     }
 }
