cefb2afb341806bef1c8d2965584a3224e852344
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-14 20:43:53 +0300

    Customer can add UPC discount and choose if each discount is applied before or after tax. There can also be extra packaging, administrative  or transport expenses in dollars or a specific rate of the orginal price

---
+++ Discount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    class Discount
+    {
+        private double universalAmount;
+        private bool universalDiscountbeforeTax;
+        private double upcAmount;
+        private bool upcDiscountbeforeTax;
+
+
+        public Discount()
+        {
+
+        }
+
+        public Discount(double universalAmount, bool universalDiscountbeforeTax, double upcAmount, bool upcDiscountbeforeTax)
+        {
+            this.universalAmount = universalAmount;
+            this.UniversalDiscountbeforeTax = universalDiscountbeforeTax;
+            this.UpcAmount = upcAmount;
+            this.UpcDiscountbeforeTax = upcDiscountbeforeTax;
+        }
+
+        public double UniversalAmount { get => universalAmount; set => universalAmount = value; }
+        public bool UniversalDiscountbeforeTax { get => universalDiscountbeforeTax; set => universalDiscountbeforeTax = value; }
+        public double UpcAmount { get => upcAmount; set => upcAmount = value; }
+        public bool UpcDiscountbeforeTax { get => upcDiscountbeforeTax; set => upcDiscountbeforeTax = value; }
+
+    }
+}
---
+++ Expenses.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    class Expenses
+    {
+        private double packagingCost;
+        private double transportCost;
+        private double packagingRate;
+        private double transportRate;
+        private double administrativeCost;
+        private double administrativeRate;
+        private string transportCostString;
+        private string packagingCostString;
+        private string administrativeCostString;
+
+
+        public double PackagingCost { get => packagingCost; set => packagingCost = value; }
+        public double TransportCost { get => transportCost; set => transportCost = value; }
+        public double PackagingRate { get => packagingRate; set => packagingRate = value; }
+        public double TransportRate { get => transportRate; set => transportRate = value; }
+        public double AdministrativeCost { get => administrativeCost; set => administrativeCost = value; }
+        public double AdministrativeRate { get => administrativeRate; set => administrativeRate = value; }
+
+        public Expenses()
+        {
+
+        }
+
+        public Expenses(string transportCostString, string packagingCostString, string administrativeCostString)
+        {
+            this.transportCostString = transportCostString;
+            this.packagingCostString = packagingCostString;
+            this.administrativeCostString = administrativeCostString;
+        }
+
+        public double CalculatePackagingCost()
+        {
+            if (packagingCostString.Contains('%'))
+            {
+                packagingCostString = packagingCostString.Replace("%", "");
+                packagingRate = Double.Parse(packagingCostString) / 100;
+                return PackagingRate / 100;
+
+            }
+            else
+            {
+                packagingCostString = packagingCostString.Replace("$", "");
+                packagingCost = Double.Parse(packagingCostString);
+                return PackagingCost;
+            }
+
+
+        }
+
+        public double CalculateTransportCost()
+        {
+            if (transportCostString.Contains('%'))
+            {
+                transportCostString = transportCostString.Replace("%", "");
+                transportRate = Double.Parse(transportCostString) / 100;
+
+                return transportRate / 100;
+
+            }
+            else
+            {
+                transportCostString = transportCostString.Replace("$", "");
+                transportCost = Double.Parse(transportCostString);
+                return transportCost;
+            }
+        }
+
+        public double CalculateAdministrativeCost()
+        {
+            if (administrativeCostString.Contains('%'))
+            {
+                administrativeCostString = administrativeCostString.Replace("%", "");
+                administrativeRate = Double.Parse(administrativeCostString) / 100;
+
+                return administrativeRate / 100;
+
+            }
+            else
+            {
+                administrativeCostString = administrativeCostString.Replace("$", "");
+                administrativeCost = Double.Parse(administrativeCostString);
+                return administrativeCost;
+            }
+        }
+    }
+}
